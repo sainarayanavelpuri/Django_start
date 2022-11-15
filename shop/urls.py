74d1718bce9349381e4a15147f57c8698ce4b310
narayana529 <sainarayana529@gmail.com>
2022-11-15 23:57:00 +0530

    apps

--- shop/urls.py
+++ shop/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import app,allproducts,store,cart,checkout
 
 urlpatterns = [
-                path('',app,name='app'),
+                path('',app,name='apps'),
                 path('allproducts',allproducts,name ='allproducts'),
                 
                 ]
